64c098b6359a38574fc39f4f245202075a648753
Anastasiia Kovalenko <anastasiia_kovalenko1@epam.com>
2022-10-25 10:13:22 +0300

    HW_06: done

---
+++ src/js/clock.js
@@ -0,0 +1,17 @@
+const showCurrentDate = () => {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const time = new Date();
+    const currentHours = (time.getHours() < 10) ? `0${time.getHours()}` : time.getHours();
+    const currentMinutes = (time.getMinutes() < 10) ? `0${time.getMinutes()}` : time.getMinutes();
+    const currentSeconds = (time.getSeconds() < 10) ? `0${time.getSeconds()}` : time.getSeconds();
+    const amPm = currentHours > 12 ? 'PM' : 'AM';
+    const currentWeekDay = weekDays[time.getDay()];
+    const currentMonth = month[time.getMonth()];
+    const currentDay = time.getDate();
+    const currentYear = time.getFullYear();
+    document.querySelector('#clock').innerHTML = 
+        `${currentHours}:${currentMinutes}:${currentSeconds} ${amPm} ${currentWeekDay}, ${currentMonth} ${currentDay}, ${currentYear}`;
+}
+
+setInterval(showCurrentDate, 1000);
---
+++ src/js/timer.js
@@ -0,0 +1,35 @@
+let timer;
+let timerStart;
+let timeOnWebsite;
+
+const startTimer = () => {
+    timerStart = Date.now();
+    timer = setInterval(() => {
+        timeOnWebsite = getTimeOnWebsite()+(Date.now()-timerStart);
+        timerStart = parseInt(Date.now());
+        document.querySelector('#timer').innerHTML = `Time: ${parseInt(timeOnWebsite/1000)}`;
+    }, 1000);
+}
+
+const getTimeOnWebsite = () => {
+    return timeOnWebsite = isNaN(timeOnWebsite) ? 0 : timeOnWebsite;
+}
+
+startTimer();
+
+document.querySelector('#timer').addEventListener('mouseover', (event) => {
+    clearInterval(timer);
+});
+
+document.querySelector('#timer').addEventListener('mouseout', (event) => {
+    startTimer();
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === "Escape") {
+        timer = 0;
+        timerStart = 0;
+        timeOnWebsite = 0;
+        startTimer();
+    }
+});
